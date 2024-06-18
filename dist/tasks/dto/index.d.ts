5f07689fd3be3d8761b99c4747b902b44d26e379
MostafaMGomaa <gomaamostafa26@gmail.com>
2024-06-18 16:39:34 +0300

    ✨ feature

--- dist/tasks/dto/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './tasks.dto';
-export * from './create-tasks.dto';
-export * from './update-tasks.dto';
--- dist/users/dto/create-user.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { UserRoles } from '../users.schema';
-export declare class CreateUserDto {
-    name: string;
-    email: string;
-    photo: string;
-    password: string;
-    role?: UserRoles.User;
-}
